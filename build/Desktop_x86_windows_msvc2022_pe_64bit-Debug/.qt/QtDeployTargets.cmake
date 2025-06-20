d3ea23e912047e5a6875558f5195b891e664bdcf
Artem Chesanovskyi <artem.chesanovskyi.ki.2022@lpnu.ua>
2025-06-20 21:49:15 +0300

    Remove build and CMakeFiles from repo

--- build/Desktop_x86_windows_msvc2022_pe_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_FlatVec-Viewer_FILE C:/Users/Артем/Desktop/FlatVec Viewer/FlatVec-Viewer/build/Desktop_x86_windows_msvc2022_pe_64bit-Debug/FlatVec-Viewer.exe)
-set(__QT_DEPLOY_TARGET_FlatVec-Viewer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_FlatVec-Viewer_RUNTIME_DLLS C:/Qt/6.8.0/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.0/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.0/msvc2022_64/bin/Qt6Cored.dll)
